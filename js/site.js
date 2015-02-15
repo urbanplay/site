0325f92f6c75a96811f6597fa8f213a507c48eb3
David A McClain <david@uhhuhyeah.com>
2015-02-15 10:38:00 -0800

    WIP - school logos

--- js/site.js
+++ js/site.js
@@ -1,3 +1,29 @@
+
+function compactCallHandlebars(
+  /*@Author: Johann Echavarria*/
+  selectorParentElement /* Parent element selector where you will append the template*/
+  ,idMainTemplate /* main template id (in script tag) */
+  ,arrParcialTemplates /*Array of parcial templates to register (ids script tag)*/
+  ,jsonData /*data*/
+  ){
+    debugger;
+    //Compile template:
+    var theTemplate = Handlebars.compile ( $( "#" + idMainTemplate ).html() ); 
+    //Add parcials with the same name as id:
+    $.each(
+      arrParcialTemplates, 
+      function( index, value ) {
+        Handlebars.registerPartial( value, $( "#" +  value ).html() );            
+      });                
+    //Append template to parent with data:                
+    $( selectorParentElement ).append( theTemplate( jsonData ) );  
+  }
+
+
 (function($){
+
+    compactCallHandlebars("#worked-with-inner", "workedWithTemplate",[], workedWithJSON);   
+
+    $('[data-toggle="tooltip"]').tooltip();
 
 })(jQuery);
